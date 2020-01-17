68789aaaeacce637ac78ec62c052be1db3da1f8b
tiancong <tiancong@longshine.com>
2020-01-17 14:19:32 +0800

    暂存

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import tab from './tab' // 页签管理模块
 // Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
+    collapse: false // 是否折叠
   },
   mutations: {
   },
--- src/store/tab.js
+++ src/store/tab.js
@@ -1,6 +1,6 @@
 export default {
   state: {
-    collapse: false // 是否折叠
+
   },
   mutations: {
 
