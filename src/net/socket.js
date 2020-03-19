1b77c8a0447f57206eba7f08bda858d92a9d5fd0
fight1991 <359193813@qq.com>
2020-03-19 13:14:59 +0800

    简易封装websocket

---
+++ src/net/socket.js
@@ -0,0 +1,62 @@
+export default class socket {
+  constructor (url) {
+    this.ws = null
+    this.url = url
+    this.isConnect = null // 是否在重新连接
+    this.timeout = 10 * 1000 // 间隔10s心跳检测
+    this.timeoutObj = null
+    this.serverTimeout = 5000 // 服务器超时
+    this.serverTimeoutObj = null
+  }
+  startLink (params, getData) {
+    this.ws = new WebSocket(this.url)
+    this.ws.onclose = (e) => { // 连接关闭触发
+      // 重新连接
+      console.log(e)
+      if (e.reason === 'destroy') return
+      this.reconnect()
+    }
+    this.ws.onerror = () => { // 发生异常
+      // 重新连接
+      this.reconnect()
+    }
+    this.ws.onopen = () => { // 连接开始
+      // 心跳检测重置
+      this.reset()
+      params && this.ws.send(params)
+    }
+    this.ws.onmessage = (res) => {
+      // 接受到服务器任何信息 说明连接没有断开,ws重新开启心跳检测
+      this.reset()
+      getData && getData(res)
+    }
+  }
+  closeLink (e) {
+    this.ws.close(e)
+  }
+  reconnect () {
+    let that = this
+    // 设置节流,避免重连请求重复
+    if (this.isConnect) return
+    this.isConnect = setTimeout(() => {
+      that.createSocket()
+      clearTimeout(that.isConnect)
+      that.isConnect = null
+    }, 4000)
+  }
+  reset () {
+    this.timeoutObj && clearInterval(this.timeoutObj)
+    this.serverTimeoutObj && clearInterval(this.serverTimeoutObj)
+    this.start()
+  }
+  start () {
+    let _this = this
+    _this.timeoutObj = setTimeout(() => { // 每x秒发送一次
+      _this.ws.send('heartBeat')
+      _this.serverTimeoutObj = setTimeout(() => { // 服务器间隔5000没有响应的话主动触发onclose
+        // 执行ws.close()会触发onclose
+        _this.ws.close()
+      }, _this.serverTimeout)
+    }, _this.timeout)
+  }
+}
