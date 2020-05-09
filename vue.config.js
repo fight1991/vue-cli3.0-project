74a0f7986a0d8800db53465f8418f7665789282d
fight1991 <359193813@qq.com>
2020-05-09 21:24:32 +0800

    配置文件修改

--- vue.config.js
+++ vue.config.js
@@ -106,7 +106,7 @@ const webpackConfig = {
 }
 if (process.env.NODE_ENV === 'production') {
   let tempCompress = new CompressionPlugin({
-    test: /\.js$|\.html$|.\css/, // 匹配文件名
+    test: /\.js$|\.html$|\.css/, // 匹配文件名
     threshold: 10240, // 对超过10k的数据压缩
     deleteOriginalAssets: false // 不删除源文件
   })
