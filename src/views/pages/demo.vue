54dea45344e6c36fda07af7721461f520e0878af
fight1991 <359193813@qq.com>
2020-05-20 13:47:04 +0800

    google地图添加maker

--- src/views/pages/demo.vue
+++ src/views/pages/demo.vue
@@ -11,19 +11,56 @@
 import google from 'google'
 export default {
   data () {
-    return {}
+    return {
+      gMap: null,
+      cuPosition: null
+    }
   },
-  created () {},
+  watch: {},
+  created () {
+    this.getGeoLocation()
+  },
   mounted () {
     this.initMap()
   },
   methods: {
     initMap () {
-      let map = new google.maps.Map(document.getElementById('map_canvas'), {
+      this.gMap = new google.maps.Map(document.getElementById('map_canvas'), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8
       })
-      console.log(map)
+    },
+    // h5获取当前经纬度信息并标识在
+    getGeoLocation () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.getLocationSuccess, this.getLocationError)
+      } else {
+        console.log('获取经纬度信息失败')
+      }
+    },
+    getLocationSuccess (res) {
+      this.cuPosition = {
+        lat: res.coords.latitude,
+        lng: res.coords.longitude
+      }
+      // 重新设置中心点
+      this.gMap.setCenter(this.cuPosition)
+      // 添加标记点
+      let marker = new google.maps.Marker({
+        position: this.cuPosition
+        // title: 'hellow'
+      })
+      marker.setMap(this.gMap)
+      // 信息窗
+      let infoWindow = new google.maps.InfoWindow()
+      infoWindow.setPosition(this.cuPosition)
+      infoWindow.setContent('Location found.')
+      marker.addListener('click', () => {
+        infoWindow.open(this.gMap, marker)
+      })
+    },
+    getLocationError () {
+      console.log('error获取经纬度信息失败')
     }
   }
 }
