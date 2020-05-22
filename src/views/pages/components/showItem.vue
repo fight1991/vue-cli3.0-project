3c3f2bbc1a2fa4c9459779351f6881b8969e27de
fight1991 <359193813@qq.com>
2020-05-22 14:35:51 +0800

    电站显示添加时钟

--- src/views/pages/components/showItem.vue
+++ src/views/pages/components/showItem.vue
@@ -45,7 +45,16 @@
       </el-card>
     </div>
     <div class="right bg-c">
-      <div class="weather-content">{{$t('common.weather')}}</div>
+      <div class="weather-content flex-around">
+        <div class="time-info">
+          <div class="time">{{timeInfo.time}}</div>
+          <div class="date">{{timeInfo.date}} {{timeInfo.week}}</div>
+        </div>
+        <div class="weather-info flex-center">
+          <div class="weather-img"><i class="iconfont icon-qing" style="font-size:60px"></i></div>
+          <div class="weather-text">22℃</div>
+        </div>
+      </div>
       <div class="map-content" style="width:100%;">
         <g-map v-if="lang=='en'"></g-map>
         <b-map v-else></b-map>
@@ -55,6 +64,7 @@
 </template>
 <script>
 import incomeItem from './incomeItem'
+import { formatDate } from '@/util'
 const bMap = () => import('./bMap')
 const gMap = () => import('./gMap')
 export default {
@@ -66,11 +76,21 @@ export default {
   },
   data () {
     return {
+      timer: null,
       plantStatus: {
         total: 0,
         normal: 0,
         abnormal: 0,
         offline: 0
+      },
+      timeInfo: {
+        date: '00-00-00',
+        time: '00:00:00',
+        week: '--'
+      },
+      timeTrans: {
+        zh: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        other: ['Monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
       },
       incomeList: [],
       mapId: '', // 地图容器 若id相同的话只渲染一次
@@ -100,7 +120,9 @@ export default {
       return this.$store.state.lang
     }
   },
-  created () {},
+  created () {
+    this.getTimeInfo()
+  },
   mounted () {},
   methods: {
     // 获取所有电站正常 非正常 故障个数
@@ -112,10 +134,27 @@ export default {
         this.plantStatus.total = result.total || 0
         this.plantStatus.normal = result.normal || 0
         this.plantStatus.abnormal = result.abnormal || 0
+        this.plantStatus.offline = result.offline || 0
       }
     },
     selectStatus (status) {
       this.$emit('getselect', status)
+    },
+    getTimeInfo () {
+      this.timer = setInterval(() => {
+        // 时:分:秒
+        let tempS = new Date()
+        let tempTime = formatDate(tempS, 'yyyy-MM-dd HH:mm:ss')
+        let tempArr = tempTime.split(' ')
+        this.timeInfo.date = tempArr[0]
+        this.timeInfo.time = tempArr[1]
+        let index = tempS.getDay() - 1
+        this.timeInfo.week = this.$store.state.lang === 'zh' ? this.timeTrans['zh'][index] : this.timeTrans['other'][index]
+      }, 1000)
+    },
+    beforeDestroy () {
+      this.timer && clearInterval(this.timer)
+      this.timer = null
     }
   }
 }
@@ -204,6 +243,20 @@ export default {
     .weather-content {
       height: 95px;
       margin-bottom: 10px;
+      text-align: center;
+      .weather-img {
+        padding: 5px 10px 5px;
+        color: #FDB201;
+      }
+      .time-info {
+        .time {
+          font-size: 40px;
+        }
+        .date {
+          padding-top: 5px;
+          font-size: 14px;
+        }
+      }
     }
     width: 350px;
     padding: 5px;
